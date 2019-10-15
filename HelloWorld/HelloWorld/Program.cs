8959b24beb1670abf2bcf007105ff79b581b3874
anthred <bak.reddy@gmail.com>
2019-10-14 20:25:08 -0700

    HelloWorld checkin

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+	static void Main()
+	{
+		int num = 2;
+		Console.WriteLine("num={0}", num);
+		
+		Console.WriteLine("ID    Name       Price");
+		Console.WriteLine("----- ---------- --------");
+		Console.WriteLine("{0,5} {1,-10} {2,8:C}", 101, "apple", .25);
+		Console.WriteLine("{0,5} {1,-10} {2,8:C}", 102, "pear", .26);
+		Console.WriteLine("{0,5} {1,-10} {2,8:C}", 4, "pineapple", 1.20);
+		Console.ReadLine();
+	}
+}
