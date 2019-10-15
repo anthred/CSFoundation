1efe03936f77bbddde0122f5612cd5fc064e7f4c
anthred <bak.reddy@gmail.com>
2019-10-14 20:28:23 -0700

    Code from VS

---
+++ Session3_Lab1/Session3_Lab1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Ascii
+{
+	static void Main()
+	{
+		Console.Write("Enter a character:");
+
+		// Let the user enter a string and press enter.
+		// code here = Console.ReadLine();
+		string input = Console.ReadLine();
+
+		// Grab the first character of the string (str[0])
+		// code here = str[0];
+		char mychar = input[0];
+
+		// Convert the character to an integer to get the ascii value
+		// code here = (int)ch;
+		int myint = (int)mychar;
+
+		Console.WriteLine("ASCII value of {0} is {1}", mychar, myint);// code here);  // show the ascii value
+		Console.ReadLine();
+	}
+}
